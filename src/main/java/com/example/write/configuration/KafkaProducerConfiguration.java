2b92269c88a9c3e65d15c2ba8d158d1f00e13564
grinn <grinnosse@gmail.com>
2022-09-06 16:27:36 +0300

    Second

--- src/main/java/com/example/write/configuration/KafkaProducerConfiguration.java
+++ src/main/java/com/example/write/configuration/KafkaProducerConfiguration.java
@@ -1,24 +1,37 @@
 package com.example.write.configuration;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfiguration {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    private String bootstrapServers = "";
+
+    public KafkaProducerConfiguration() {
+        File file = new File("properties/kafka.properties");
+        try(FileReader reader = new FileReader(file))
+        {
+            int c;
+            while((c=reader.read())!=-1){
+                bootstrapServers = bootstrapServers + (char)c;
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 
     public Map<String, Object> producerConfig() {
         Map<String, Object> props = new HashMap<>();
